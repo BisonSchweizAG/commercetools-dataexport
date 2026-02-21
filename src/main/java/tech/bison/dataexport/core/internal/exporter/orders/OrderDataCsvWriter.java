d4175d83a1a49c56d959cbd31de5b779d05dcc4d
Marco Studer <marco.studer@gmx.net>
2026-02-21 10:50:33 +0100

    Refactor line item attribute extraction in order CSV writer
    Signed-off-by: Marco Studer <marco.studer@gmx.net>

--- src/main/java/tech/bison/dataexport/core/internal/exporter/orders/OrderDataCsvWriter.java
+++ src/main/java/tech/bison/dataexport/core/internal/exporter/orders/OrderDataCsvWriter.java
@@ -20,132 +20,130 @@ import com.commercetools.api.models.common.CentPrecisionMoney;
 import com.commercetools.api.models.order.Order;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Stream;
 import org.apache.commons.csv.CSVPrinter;
 import tech.bison.dataexport.core.api.configuration.DataExportProperties;
 import tech.bison.dataexport.core.api.exception.DataExportException;
 import tech.bison.dataexport.core.api.executor.DataWriter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public class OrderDataCsvWriter implements DataWriter {
 
-  private static final String LINE_ITEM_PREFIX = "lineItems.";
-  private static final String VARIANT_ATTRIBUTES_PREFIX = "variant.attributes.";
-  private final CSVPrinter csvPrinter;
-  private final DataExportProperties dataExportProperties;
-  private final ObjectMapper objectMapper;
+    private static final String LINE_ITEM_PREFIX = "lineItems.";
+    private static final String VARIANT_ATTRIBUTES_PREFIX = "variant.attributes.";
+    private final CSVPrinter csvPrinter;
+    private final DataExportProperties dataExportProperties;
+    private final ObjectMapper objectMapper;
 
-  public OrderDataCsvWriter(CSVPrinter csvPrinter, DataExportProperties dataExportProperties,
-      ObjectMapper objectMapper) {
-    this.csvPrinter = csvPrinter;
-    this.dataExportProperties = dataExportProperties;
-    this.objectMapper = objectMapper;
-  }
-
-  @Override
-  public void writeRow(BaseResource source) {
-    Order order = (Order) source;
-    JsonNode node = objectMapper.valueToTree(source);
-
-    var orderFields = dataExportProperties.fields().stream().filter(field -> !field.startsWith(LINE_ITEM_PREFIX))
-        .toList();
-    var lineItemFields = dataExportProperties.fields().stream().filter(field -> field.startsWith(LINE_ITEM_PREFIX))
-        .toList();
-    if (!lineItemFields.isEmpty()) {
-      writeRecordsWithLineItems(order, node, orderFields, lineItemFields);
-    } else {
-      List<String> values = new ArrayList<>();
-      for (String field : dataExportProperties.fields()) {
-        values.add(extractValue(node, field));
-      }
-      writeRecord(order, values);
-    }
-  }
-
-  private void writeRecordsWithLineItems(Order order, JsonNode orderNode, List<String> orderFields,
-      List<String> lineItemFields) {
-    var orderRecord = Stream.concat(
-        orderFields.stream().map(f -> extractValue(orderNode, f)),
-        Collections.nCopies(lineItemFields.size(), "").stream()
-    ).toList();
-    writeRecord(order, orderRecord);
-
-    for (var lineItem : order.getLineItems()) {
-      JsonNode lineItemNode = objectMapper.valueToTree(lineItem);
-      var lineItemRecord = Stream.concat(
-          Collections.nCopies(orderFields.size(), "").stream(),
-          lineItemFields.stream().map(f -> extractLineItemValue(lineItemNode, f.replace(LINE_ITEM_PREFIX, "")))
-      ).toList();
-      writeRecord(order, lineItemRecord);
-    }
-  }
-
-  private String extractLineItemValue(JsonNode lineItemNode, String field) {
-    if (!field.startsWith(VARIANT_ATTRIBUTES_PREFIX)) {
-      return extractValue(lineItemNode, field);
+    public OrderDataCsvWriter(CSVPrinter csvPrinter, DataExportProperties dataExportProperties,
+                              ObjectMapper objectMapper) {
+        this.csvPrinter = csvPrinter;
+        this.dataExportProperties = dataExportProperties;
+        this.objectMapper = objectMapper;
     }
 
-    String attributePath = field.substring(VARIANT_ATTRIBUTES_PREFIX.length());
-    if (attributePath.isBlank()) {
-      return "";
+    @Override
+    public void writeRow(BaseResource source) {
+        Order order = (Order) source;
+        JsonNode node = objectMapper.valueToTree(source);
+
+        var orderFields = dataExportProperties.fields().stream().filter(field -> !field.startsWith(LINE_ITEM_PREFIX))
+                .toList();
+        var lineItemFields = dataExportProperties.fields().stream().filter(field -> field.startsWith(LINE_ITEM_PREFIX))
+                .toList();
+        if (!lineItemFields.isEmpty()) {
+            writeRecordsWithLineItems(order, node, orderFields, lineItemFields);
+        } else {
+            List<String> values = new ArrayList<>();
+            for (String field : dataExportProperties.fields()) {
+                values.add(extractValue(node, field));
+            }
+            writeRecord(order, values);
+        }
     }
 
-    String attributeName = attributePath;
-    String nestedPath = null;
-    int nestedPathSeparatorIdx = attributePath.indexOf('.');
-    if (nestedPathSeparatorIdx >= 0) {
-      attributeName = attributePath.substring(0, nestedPathSeparatorIdx);
-      nestedPath = attributePath.substring(nestedPathSeparatorIdx + 1);
+    private void writeRecordsWithLineItems(Order order, JsonNode orderNode, List<String> orderFields,
+                                           List<String> lineItemFields) {
+        var orderRecord = Stream.concat(
+                orderFields.stream().map(f -> extractValue(orderNode, f)),
+                Collections.nCopies(lineItemFields.size(), "").stream()
+        ).toList();
+        writeRecord(order, orderRecord);
+
+        for (var lineItem : order.getLineItems()) {
+            JsonNode lineItemNode = objectMapper.valueToTree(lineItem);
+            var lineItemRecord = Stream.concat(
+                    Collections.nCopies(orderFields.size(), "").stream(),
+                    lineItemFields.stream().map(f -> extractLineItemValue(lineItemNode, f.replace(LINE_ITEM_PREFIX, "")))
+            ).toList();
+            writeRecord(order, lineItemRecord);
+        }
     }
 
-    JsonNode attributes = lineItemNode.at("/variant/attributes");
+    private String extractLineItemValue(JsonNode lineItemNode, String field) {
+        if (!field.startsWith(VARIANT_ATTRIBUTES_PREFIX)) {
+            return extractValue(lineItemNode, field);
+        }
 
-    for (JsonNode attribute : attributes) {
-      if (!attributeName.equals(attribute.path("name").asText())) {
-        continue;
-      }
+        String attributePath = field.substring(VARIANT_ATTRIBUTES_PREFIX.length());
+        String attributeName = attributePath;
+        String nestedPath = null;
+        int nestedPathSeparatorIdx = attributePath.indexOf('.');
+        if (nestedPathSeparatorIdx >= 0) {
+            attributeName = attributePath.substring(0, nestedPathSeparatorIdx);
+            nestedPath = attributePath.substring(nestedPathSeparatorIdx + 1);
+        }
+        final String targetAttributeName = attributeName;
+        final String targetNestedPath = nestedPath;
 
-      JsonNode attributeValue = attribute.path("value");
-      if (nestedPath == null) {
-        return extractNodeValue(attributeValue);
-      }
-      return extractValue(attributeValue, nestedPath);
+        JsonNode attributes = lineItemNode.at("/variant/attributes");
+        return StreamSupport.stream(attributes.spliterator(), false)
+                .filter(attribute -> targetAttributeName.equals(attribute.path("name").asText()))
+                .findFirst()
+                .map(attribute -> {
+                    JsonNode attributeValue = attribute.path("value");
+                    if (targetNestedPath == null) {
+                        return extractNodeValue(attributeValue);
+                    }
+                    return extractValue(attributeValue, targetNestedPath);
+                })
+                .orElse("");
     }
-    return "";
-  }
 
-  private void writeRecord(Order order, List<String> values) {
-    try {
-      csvPrinter.printRecord(values);
-    } catch (IOException e) {
-      throw new DataExportException(String.format("Could not write order '%s'", order.getId()), e);
+    private void writeRecord(Order order, List<String> values) {
+        try {
+            csvPrinter.printRecord(values);
+        } catch (IOException e) {
+            throw new DataExportException(String.format("Could not write order '%s'", order.getId()), e);
+        }
     }
-  }
 
-  @Override
-  public void flush() {
-    try {
-      csvPrinter.flush();
-    } catch (IOException e) {
-      throw new DataExportException("Could not flush order csv writer.", e);
+    @Override
+    public void flush() {
+        try {
+            csvPrinter.flush();
+        } catch (IOException e) {
+            throw new DataExportException("Could not flush order csv writer.", e);
+        }
     }
-  }
 
-  private String extractValue(JsonNode node, String field) {
-    String pointer = "/" + field.replace(".", "/");
-    JsonNode value = node.at(pointer);
-    return extractNodeValue(value);
-  }
-
-  private String extractNodeValue(JsonNode value) {
-    if (value.get("type") != null && CentPrecisionMoney.CENT_PRECISION.equals(value.get("type").asText())) {
-      double amount = value.get("centAmount").asInt() / 100d;
-      return String.valueOf(amount);
-    } else {
-      return value.asText("");
+    private String extractValue(JsonNode node, String field) {
+        String pointer = "/" + field.replace(".", "/");
+        JsonNode value = node.at(pointer);
+        return extractNodeValue(value);
     }
-  }
+
+    private String extractNodeValue(JsonNode value) {
+        if (value.get("type") != null && CentPrecisionMoney.CENT_PRECISION.equals(value.get("type").asText())) {
+            double amount = value.get("centAmount").asInt() / 100d;
+            return String.valueOf(amount);
+        } else {
+            return value.asText("");
+        }
+    }
 }
