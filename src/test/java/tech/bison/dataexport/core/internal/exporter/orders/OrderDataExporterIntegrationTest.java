3ebf792e4b4066a99bd78d6f6b0bf802c0c6db31
Marco Studer <marco.studer@gmx.net>
2026-02-21 13:55:44 +0100

    Implement customer data exporter with paging
    Signed-off-by: Marco Studer <marco.studer@gmx.net>

--- src/test/java/tech/bison/dataexport/core/internal/exporter/orders/OrderDataExporterIntegrationTest.java
+++ src/test/java/tech/bison/dataexport/core/internal/exporter/orders/OrderDataExporterIntegrationTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2000 - 2026 Bison Schweiz AG
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package tech.bison.dataexport.core.internal.exporter.orders;
 
 import com.commercetools.api.models.order.Order;
@@ -17,7 +29,7 @@ import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
 @WireMockTest(httpPort = 8087)
-class DataExporterIntegrationTest {
+class OrderDataExporterIntegrationTest {
     private Context context;
 
     @BeforeEach
